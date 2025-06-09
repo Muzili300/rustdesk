42646b1315ea1bcf5c16834780e29c17ef0d569a
Muzili300 <13051434996@163.com>
2025-06-09 11:10:16 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://192.168.60.41:21114".to_owned()
 }
 
 #[inline]
